be5ad75ba12a342ee05aaab6bc78bd4603afd819
Antonio Rodríguez Alcaide <omegados@hotmail.com>
2021-07-26 21:28:58 +0200

    Cambio de version a 2

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version v2 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is v2 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
